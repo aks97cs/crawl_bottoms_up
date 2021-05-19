24efe2256e0a4e47da42e87543032e5316aaa704
Anuj Kumar Singh <anujs@zenarate.com>
2021-05-19 18:34:15 +0530

    added example for crawling image url

--- crawl_bottoms_up/spiders/image_spider.py
+++ crawl_bottoms_up/spiders/image_spider.py
@@ -1,12 +1,25 @@
 import scrapy
+import json
 
 
 class ImageSpider(scrapy.Spider):
 
-    name = 'image crawler'
+    name = 'image_crawler'
 
     def start_requests(self):
-        pass
+        urls = [
+            "https://unsplash.com/s/photos/dogs"
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
-        pass
+
+        res = response.css("img::attr(src)").extract()
+
+        # collector is the directory name should be available in root dir of the project
+
+        with open('collector/out.json', 'wb') as f:
+            f.write((json.dumps(res).encode()))
+        # print(json.dumps(res))
